6932f9e7fc1526bd377f96901625e11cf64adf44
mayali123 <59428484+mayali123@users.noreply.github.com>
2020-10-09 14:05:54 +0800

    Update 2020年10月8号.py

--- python的作业/2020年10月8号.py
+++ python的作业/2020年10月8号.py
@@ -1,20 +1,30 @@
+# 93页3.2
+time = 365
+capacity = 1
+b = 0
+for i in range(1, time+1):
+    if i % 7 not in [1, 2, 3]:
+        capacity *= 1.01
+print("连续学天%d，365天后的能力值为%f" % (365, capacity))
 
-st = input("请输入一个五位数：")
-if st[:3] == st[:-4:-1]:
+# 93页3.3
+time = 365
+num = [10, 15]
+for k in num:
+    capacity = 1
+    b = 0
+    for i in range(1, time+1):
+        if i % (k+1) != 0:
+            if (i-(k+1)*b) % 7 not in [1, 2, 3]:
+                capacity *= 1.01
+        else:
+            b += 1
+    print("连续学天%d，休息1天，365天后的能力值为%f" % (k, capacity))
+
+
+# 93页3.4
+st = input("请输入一个数：")
+if st == st[::-1]:
     print("这是一个回文数")
 else:
     print("这不是一个回文数")
-
-
-#time 连续学习的天数
-time = 365
-after_up = (1+0.01)**(time-3)
-print('连续学'+str(time)+'天，持续学365天之后之后的能力值为'+str(after_up))
-
-
-# time 连续学习的天数
-time = 10
-after_up = ((1+0.01)**(time-3))**(365//10)
-remainder = 365 % 10
-after_up += (1+0.01)**(remainder-3)
-print('连续学'+str(time)+'天，持续学365天之后的能力值为'+str(after_up))
