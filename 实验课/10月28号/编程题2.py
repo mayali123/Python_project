6fc8188fc9168fdea56d78b51827e13c11c14630
mayali123 <59428484+mayali123@users.noreply.github.com>
2020-10-28 11:41:38 +0800

    Add files via upload

--- 实验课/10月28号/编程题2.py
+++ 实验课/10月28号/编程题2.py
@@ -3,7 +3,11 @@ random.seed(1)
 num = random.randint(0, 100)
 i = 0
 while 1:
-    guess_num = int(input("请猜一个在0~100的数:"))
+    st = eval(input("请猜一个在0~100的数:"))
+    while st%1!=0:
+        print('输入内容必须为整数！')
+        st = eval(input("请猜一个在0~100的数:"))
+    guess_num = st
     i += 1
     if guess_num == num:
         print("预测{}次，你猜中了！".format(i))
